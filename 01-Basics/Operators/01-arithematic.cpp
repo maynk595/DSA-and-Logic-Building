4b28c0081053d504b52200eed3a3e7738bed2df3
Mayank Rawat <rawatmayank5662@gmail.com>
2026-09-03 11:31:14 +0530

    Add basic arithmetic operations in C++
    This program performs basic arithmetic operations on two integers.

---
+++ 01-Basics/Operators/01-arithematic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2;
+  cin>>n1>>n2;
+  cout<<n1+n2<<endl;
+  cout<<n1-n2<<endl;
+  cout<<n1*n2<<endl;
+  cout<<n1/n2<<endl; // do not use n2=0 
+  cout<<n1%n2<<endl;  // do not use n2=0 
+  return 0;
+ }
