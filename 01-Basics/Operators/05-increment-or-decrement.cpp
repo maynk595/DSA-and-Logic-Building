d70567fd0d5d3cc5846ba88d0dc0aebbf2cc17d1
maynk595 <rawatmayank5662@gmail.com>
2026-09-07 22:05:14 +0530

    Add increment and decrement operator

---
+++ 01-Basics/Operators/05-increment-or-decrement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    a++; // a = a + 1
+    cout<<a<<endl;
+    a--; // a = a - 1
+    cout<<a<<endl;
+    return 0;
+}
