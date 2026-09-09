578b88a587ae9bdf3f2fab58ad4c734b3a272a34
maynk595 <rawatmayank5662@gmail.com>
2026-09-09 09:44:55 +0530

    Add ternary operator

---
+++ 01-Basics/Operators/07-ternary.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<(a>b ? a : b)<<endl; // ternary operator (if condition is true then it will return first value otherwise second value)
+    return 0;
+}
